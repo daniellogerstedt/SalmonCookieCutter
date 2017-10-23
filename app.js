4f7657145584c22e699c46f8779793fdb71c2be5
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-10-23 15:29:34 -0700

    finished first object method for generating hourly sales

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 'use strict';
+
 // 1st and Pike Store Object
 var firstPikeStore = {
   minCust: 23,
   maxCust: 65,
-  avgCookSale: 6.3
+  avgCookSale: 6.3,
+  hoursOfBusiness: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  salesPerHour: [],
+  actualCust: function() {
+    var min = this.minCust;
+    var max = this.maxCust;
+    var salesAmounts = [];
+    for (var i = 0; i < this.hoursOfBusiness.length; i++){
+      var randomSale = Math.floor(Math.random() * (max - min) + min );
+      // creates array information for hourly sales rounding up to ensure there are never too few salmon cookies.
+      salesAmounts.push(Math.ceil(randomSale * this.avgCookSale));
+    }
+    this.salesPerHour = salesAmounts;
+    return;
+  }
 };
 
 // SeaTac Airport Store Object
