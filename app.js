a52d52eee3749ee21f78a328f471718870bcbd88
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-10-23 14:47:38 -0700

    built objects for stores

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 'use strict';
+// 1st and Pike Store Object
+var firstPikeStore = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookSale: 6.3
+};
+
+// SeaTac Airport Store Object
+var seatacAirportStore = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookSale: 1.2
+};
+
+// Seattle Center Store Object
+var seattleCenterStore = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookSale: 3.7
+};
+
+// Capitol Hill Store Object
+var capitolHillStore = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookSale: 2.3
+};
+
+// Alki Store Object
+var alkiBeachStore = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookSale: 4.6
+};
